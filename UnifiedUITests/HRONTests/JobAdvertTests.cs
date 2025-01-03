3c274230fa8643a04c1081c6e08f12154b039a1d
Freakkoe <mads.g.j@live.dk>
2025-01-03 01:57:15 +0100

    Test Project with TDD on some CRUD elements and other tests implemented.

---
+++ UnifiedUITests/HRONTests/JobAdvertTests.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using HRONSystem.Data;
+using HRONSystem.Models;
+
+namespace UnifiedUITests.HRONTests
+{
+    [TestClass]
+    public class JobAdvertCRUDTests
+    {
+        private HRONNewDbContext _context;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            // In-memory database setup
+            var options = new DbContextOptionsBuilder<HRONNewDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) // New Database with each test 
+                .Options;
+
+            _context = new HRONNewDbContext(options);
+
+            // Clears Database
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+
+            Assert.AreEqual(0, _context.JobAdverts.Count(), "Database is not empty after initialization");
+        }
+
+
+
+        [TestMethod]
+        public void CreateJobAdvert_ShouldAddToDatabase()
+        {
+            // Arrange
+            var jobAdvert = new JobAdvert
+            {
+                PublishingDate = DateTime.Now,
+                ApplicationDeadline = DateTime.Now.AddDays(7),
+                JobTitle = "Test Job",
+                Media = "Online", 
+                Status = "Active",
+                Responsible = "John Doe",
+                Department = "IT", 
+                Views = 10,
+                Applications = 5,
+                Comments = "This is a test comment",
+                IsArchived = false
+            };
+
+            // Act
+            _context.JobAdverts.Add(jobAdvert);
+            _context.SaveChanges();
+
+            // Assert
+            var jobAdverts = _context.JobAdverts.ToList();
+            Assert.AreEqual(1, jobAdverts.Count, "Number of job adverts in the database is incorrect");
+            Assert.AreEqual("Test Job", jobAdverts.First().JobTitle);
+        }
+
+
+
+        [TestMethod]
+        public void ReadJobAdvert_ShouldRetrieveFromDatabase()
+        {
+            // Arrange
+            var jobAdvert = new JobAdvert
+            {
+                PublishingDate = DateTime.Now,
+                ApplicationDeadline = DateTime.Now.AddDays(7),
+                JobTitle = "Test Job",
+                Media = "Online",
+                Status = "Active",
+                Responsible = "John Doe",
+                Department = "IT",
+                Views = 10,
+                Applications = 5,
+                Comments = "This is a test comment",
+                IsArchived = false
+            };
+
+            _context.JobAdverts.Add(jobAdvert);
+            _context.SaveChanges();
+
+            // Act
+            var retrievedJob = _context.JobAdverts.FirstOrDefault(j => j.JobTitle == "Test Job");
+
+            // Assert
+            Assert.IsNotNull(retrievedJob, "Job advert was not retrieved.");
+            Assert.AreEqual("John Doe", retrievedJob.Responsible, $"Expected Responsible: John Doe, but got {retrievedJob.Responsible}");
+            Assert.AreEqual(10, retrievedJob.Views, $"Expected Views: 10, but got {retrievedJob.Views}");
+        }
+
+
+        [TestMethod]
+        public void UpdateJobAdvert_ShouldModifyExistingRecord()
+        {
+            // Arrange
+            var jobAdvert = new JobAdvert
+            {
+                PublishingDate = DateTime.Now,
+                ApplicationDeadline = DateTime.Now.AddDays(7),
+                JobTitle = "Test Job",
+                Media = "Online",
+                Status = "Active",
+                Responsible = "John Doe",
+                Department = "IT",
+                Views = 10,
+                Applications = 5,
+                Comments = "This is a test comment",
+                IsArchived = false
+            };
+
+            _context.JobAdverts.Add(jobAdvert);
+            _context.SaveChanges();
+
+            // Act
+            var existingJob = _context.JobAdverts.First();
+            existingJob.Status = "Inactive";
+            _context.SaveChanges();
+
+            // Assert
+            Assert.AreEqual("Inactive", _context.JobAdverts.First().Status);
+        }
+
+        [TestMethod]
+        public void DeleteJobAdvert_ShouldRemoveFromDatabase()
+        {
+            // Arrange
+            var jobAdvert = new JobAdvert
+            {
+                PublishingDate = DateTime.Now,
+                ApplicationDeadline = DateTime.Now.AddDays(7),
+                JobTitle = "Test Job",
+                Media = "Online",
+                Status = "Active",
+                Responsible = "John Doe",
+                Department = "IT",
+                Views = 10,
+                Applications = 5,
+                Comments = "This is a test comment",
+                IsArchived = false
+            };
+
+            _context.JobAdverts.Add(jobAdvert);
+            _context.SaveChanges();
+
+            // Act
+            var existingJob = _context.JobAdverts.First();
+            _context.JobAdverts.Remove(existingJob);
+            _context.SaveChanges();
+
+            // Assert
+            Assert.AreEqual(0, _context.JobAdverts.Count(), "Database should be empty after deletion");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _context.Dispose();
+        }
+    }
+}
+
---
+++ UnifiedUITests/SDLoenTests/EmployeeTests.cs
@@ -0,0 +1,188 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SDLoenSystem.Data;
+using SDLoenSystem.Models;
+using System;
+using System.Linq;
+
+namespace UnifiedUITests.SDLoenTests
+{
+    [TestClass]
+    public class EmployeeInfoCRUDTests
+    {
+        private SDLOENDbContext _context;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            // In-memory database setup
+            var options = new DbContextOptionsBuilder<SDLOENDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new SDLOENDbContext(options);
+
+            // Clear database
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+        }
+
+        [TestMethod]
+        public void CreateEmployee_ShouldAddToDatabase()
+        {
+            // Arrange
+            var employee = new EmployeeInfo
+            {
+                EmploymentDate = "2024-01-01",
+                CPRNumber = "1234567890",
+                FirstName = "John",
+                LastName = "Doe",
+                Address = "123 Street",
+                Position = "Developer",
+                WorkHours = 37,
+                Institution = "Tech Department",
+                JobType = "Full-time",
+                Department = "IT",
+                AuthorizationRequirement = "None",
+                PhoneWork = "12345678",
+                PhonePrivate = "87654321",
+                EmailWork = "john.doe@work.com",
+                EmailPrivate = "john.doe@gmail.com",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddYears(1),
+                LastUpdated = DateTime.Now,
+                CreationDate = DateTime.Now
+            };
+
+            // Act
+            _context.EmployeeInfos.Add(employee);
+            _context.SaveChanges();
+
+            // Assert
+            Assert.AreEqual(1, _context.EmployeeInfos.Count(), "Number of employees in the database is incorrect.");
+            Assert.AreEqual("John", _context.EmployeeInfos.First().FirstName);
+        }
+
+        [TestMethod]
+        public void ReadEmployee_ShouldRetrieveFromDatabase()
+        {
+            // Arrange
+            var employee = new EmployeeInfo
+            {
+                EmploymentDate = "2024-01-01",
+                CPRNumber = "1234567890",
+                FirstName = "John",
+                LastName = "Doe",
+                Address = "123 Street",
+                Position = "Developer",
+                WorkHours = 37,
+                Institution = "Tech Department",
+                JobType = "Full-time",
+                Department = "IT",
+                AuthorizationRequirement = "None",
+                PhoneWork = "12345678",
+                PhonePrivate = "87654321",
+                EmailWork = "john.doe@work.com",
+                EmailPrivate = "john.doe@gmail.com",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddYears(1),
+                LastUpdated = DateTime.Now,
+                CreationDate = DateTime.Now
+            };
+
+            _context.EmployeeInfos.Add(employee);
+            _context.SaveChanges();
+
+            // Act
+            var retrievedEmployee = _context.EmployeeInfos.FirstOrDefault(e => e.FirstName == "John");
+
+            // Assert
+            Assert.IsNotNull(retrievedEmployee, "Employee was not found in the database.");
+            Assert.AreEqual("123 Street", retrievedEmployee.Address);
+        }
+
+        [TestMethod]
+        public void UpdateEmployee_ShouldModifyExistingRecord()
+        {
+            // Arrange
+            var employee = new EmployeeInfo
+            {
+                EmploymentDate = "2024-01-01",
+                CPRNumber = "1234567890",
+                FirstName = "John",
+                LastName = "Doe",
+                Address = "123 Street",
+                Position = "Developer",
+                WorkHours = 37,
+                Institution = "Tech Department",
+                JobType = "Full-time",
+                Department = "IT",
+                AuthorizationRequirement = "None",
+                PhoneWork = "12345678",
+                PhonePrivate = "87654321",
+                EmailWork = "john.doe@work.com",
+                EmailPrivate = "john.doe@gmail.com",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddYears(1),
+                LastUpdated = DateTime.Now,
+                CreationDate = DateTime.Now
+            };
+
+            _context.EmployeeInfos.Add(employee);
+            _context.SaveChanges();
+
+            // Act
+            var existingEmployee = _context.EmployeeInfos.First();
+            existingEmployee.Position = "Senior Developer";
+            _context.SaveChanges();
+
+            // Assert
+            Assert.AreEqual("Senior Developer", _context.EmployeeInfos.First().Position);
+        }
+
+        [TestMethod]
+        public void DeleteEmployee_ShouldRemoveFromDatabase()
+        {
+            // Arrange
+            var employee = new EmployeeInfo
+            {
+                EmploymentDate = "2024-01-01",
+                CPRNumber = "1234567890",
+                FirstName = "John",
+                LastName = "Doe",
+                Address = "123 Street",
+                Position = "Developer",
+                WorkHours = 37,
+                Institution = "Tech Department",
+                JobType = "Full-time",
+                Department = "IT",
+                AuthorizationRequirement = "None",
+                PhoneWork = "12345678",
+                PhonePrivate = "87654321",
+                EmailWork = "john.doe@work.com",
+                EmailPrivate = "john.doe@gmail.com",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddYears(1),
+                LastUpdated = DateTime.Now,
+                CreationDate = DateTime.Now
+            };
+
+            _context.EmployeeInfos.Add(employee);
+            _context.SaveChanges();
+
+            // Act
+            var existingEmployee = _context.EmployeeInfos.First();
+            _context.EmployeeInfos.Remove(existingEmployee);
+            _context.SaveChanges();
+
+            // Assert
+            Assert.AreEqual(0, _context.EmployeeInfos.Count(), "Database should be empty after deletion.");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _context.Dispose();
+        }
+    }
+}
