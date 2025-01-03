5016bf2aa0f3113cc5f7f044b89413d490ad9e69
Freakkoe <mads.g.j@live.dk>
2025-01-03 03:54:44 +0100

    Multiple Active Database connections Successful. Added create button to unifiedUI HRON interface WORKING!!

---
+++ UnifiedUISystem/Pages/HRON/Create.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using HRONSystem.Data;
+using HRONSystem.Models;
+
+namespace UnifiedUISystem.Pages.HRON
+{
+    public class CreateModel : PageModel
+    {
+        private readonly HRONNewDbContext _context;
+
+        public CreateModel(HRONNewDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public JobAdvert JobAdvert { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.JobAdverts.Add(JobAdvert);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("../Index");
+        }
+    }
+}
+
--- UnifiedUISystem/Program.cs
+++ UnifiedUISystem/Program.cs
@@ -1,12 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using UnifiedUISystem.Data;
+using HRONSystem.Data;
+using SDLoenSystem.Data;
+using SofdCoreSystem.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Adds DbContexts for each database
-builder.Services.AddDbContext<HRONNewDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("HRONConnection")));
-builder.Services.AddDbContext<SDLOENDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SDLOENConnection")));
-builder.Services.AddDbContext<SofdCoreDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SofdCoreConnection")));
+builder.Services.AddDbContext<HRONSystem.Data.HRONNewDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("HRONConnection")));
+builder.Services.AddDbContext<UnifiedUISystem.Data.HRONNewDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("HRONConnection")));
+builder.Services.AddDbContext<SDLoenSystem.Data.SDLOENDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SDLOENConnection")));
+builder.Services.AddDbContext<UnifiedUISystem.Data.SDLOENDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SDLOENConnection")));
+builder.Services.AddDbContext<SofdCoreSystem.Data.SofdCoreDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SofdCoreConnection")));
+builder.Services.AddDbContext<UnifiedUISystem.Data.SofdCoreDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SofdCoreConnection")));
 
 // Add services to the container.
 builder.Services.AddRazorPages();
