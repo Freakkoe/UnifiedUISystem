d2f243d8476c9f88996fb1120cc5523a70fda420
Freakkoe <mads.g.j@live.dk>
2025-01-10 22:45:29 +0100

    Changes to Edit in SDLoenSystem as of now works with different cshtml

--- SDLoenSystem/Pages/EmployeeInfo/Delete.cshtml.cs
+++ SDLoenSystem/Pages/EmployeeInfo/Delete.cshtml.cs
@@ -1,62 +1,62 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.RazorPages;
-//using Microsoft.EntityFrameworkCore;
-//using SDLoenSystem.Data;
-//using SDLoenSystem.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SDLoenSystem.Data;
+using SDLoenSystem.Models;
 
-//namespace SDLoenSystem.Pages.EmployeeInfo
-//{
-//    public class DeleteModel : PageModel
-//    {
-//        private readonly SDLoenSystem.Data.SDLOENDbContext _context;
+namespace SDLoenSystem.Pages.EmployeeInfo
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SDLoenSystem.Data.SDLOENDbContext _context;
 
-//        public DeleteModel(SDLoenSystem.Data.SDLOENDbContext context)
-//        {
-//            _context = context;
-//        }
+        public DeleteModel(SDLoenSystem.Data.SDLOENDbContext context)
+        {
+            _context = context;
+        }
 
-//        [BindProperty]
-//        public Models.EmployeeInfo EmployeeInfo { get; set; } = default!;
+        [BindProperty]
+        public Models.EmployeeInfo EmployeeInfo { get; set; } = default!;
 
-//        public async Task<IActionResult> OnGetAsync(int? id)
-//        {
-//            if (id == null)
-//            {
-//                return NotFound();
-//            }
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-//            var employeeinfo = await _context.EmployeeInfos.FirstOrDefaultAsync(m => m.Id == id);
+            var employeeinfo = await _context.EmployeeInfos.FirstOrDefaultAsync(m => m.Id == id);
 
-//            if (employeeinfo is not null)
-//            {
-//                EmployeeInfo = employeeinfo;
+            if (employeeinfo is not null)
+            {
+                EmployeeInfo = employeeinfo;
 
-//                return Page();
-//            }
+                return Page();
+            }
 
-//            return NotFound();
-//        }
+            return NotFound();
+        }
 
-//        public async Task<IActionResult> OnPostAsync(int? id)
-//        {
-//            if (id == null)
-//            {
-//                return NotFound();
-//            }
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-//            var employeeinfo = await _context.EmployeeInfos.FindAsync(id);
-//            if (employeeinfo != null)
-//            {
-//                EmployeeInfo = employeeinfo;
-//                _context.EmployeeInfos.Remove(EmployeeInfo);
-//                await _context.SaveChangesAsync();
-//            }
+            var employeeinfo = await _context.EmployeeInfos.FindAsync(id);
+            if (employeeinfo != null)
+            {
+                EmployeeInfo = employeeinfo;
+                _context.EmployeeInfos.Remove(EmployeeInfo);
+                await _context.SaveChangesAsync();
+            }
 
-//            return RedirectToPage("./Index");
-//        }
-//    }
-//}
+            return RedirectToPage("./Index");
+        }
+    }
+}
